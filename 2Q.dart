763ffef96a9905a584fbbdf3c6604f11beb05995
MohammadAli0096 <mohammadaliattari92@gmail.com>
2026-09-11 17:41:17 +0500

    for in loop

---
+++ 2Q.dart
@@ -0,0 +1,15 @@
+// 2. Create a list of 5 names and print each name. (for-in loop)
+void main() {
+  List<String> names = [
+    "Ali",
+    "Ahmed",
+    "Ayesha",
+    "Fatima",
+    "Hassan",
+    "Bilal",
+    "Usman",
+  ];
+  for (String name in names) {
+    print(name);
+  }
+}
